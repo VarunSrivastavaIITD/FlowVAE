526588ece5eedf2325fd4e0c39bf9e54a19239fa
Varun Srivastava <varunsrivastava.v@gmail.com>
2019-11-09 19:05:06 -0800

    Reformat

--- models.py
+++ models.py
@@ -4,123 +4,134 @@ import torch.nn.functional as F
 from torch import autograd, nn, optim
 from torch.nn import functional as F
 
-class Encoder(nn.Module):
-	"""docstring for Encoder"""
-	def __init__(self, x_dim, z_dim, n_units):
-		super().__init__()
-		self.x_dim = x_dim
-		self.z_dim = z_dim
-		self.n_units = n_units
-		layers = [nn.Linear(x_dim, n_units[0]), nn.ELU()]
-		for i in range(1,len(n_units)):
-			layers.append(nn.Linear(n_units[i-1],n_units[i]))
-			layers.append(nn.ELU())
-		layers.append(nn.Linear(n_units[-1],z_dim))
-		self.net = nn.Sequential(*layers)
 
-	def forward(self,x):
-		return self.net(x)
+class Encoder(nn.Module):
+    """docstring for Encoder"""
+
+    def __init__(self, x_dim, z_dim, n_units):
+        super().__init__()
+        self.x_dim = x_dim
+        self.z_dim = z_dim
+        self.n_units = n_units
+        layers = [nn.Linear(x_dim, n_units[0]), nn.ELU()]
+        for i in range(1, len(n_units)):
+            layers.append(nn.Linear(n_units[i - 1], n_units[i]))
+            layers.append(nn.ELU())
+        layers.append(nn.Linear(n_units[-1], z_dim))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
+
 
 class Decoder(nn.Module):
-	def __init__(self, z_dim, x_dim, n_units, x_type='binary'):
-		super().__init__()
-		self.x_dim = x_dim
-		self.z_dim = z_dim
-		self.n_units = n_units
-		layers = [nn.Linear(z_dim, n_units[0]), nn.ELU()]
-		for i in range(1,len(n_units)):
-			layers.append(nn.Linear(n_units[i-1],n_units[i]))
-			layers.append(nn.ELU())
-		layers.append(nn.Linear(n_units[-1],x_dim))
-		self.net = nn.Sequential(*layers)
+    def __init__(self, z_dim, x_dim, n_units, x_type="binary"):
+        super().__init__()
+        self.x_dim = x_dim
+        self.z_dim = z_dim
+        self.n_units = n_units
+        layers = [nn.Linear(z_dim, n_units[0]), nn.ELU()]
+        for i in range(1, len(n_units)):
+            layers.append(nn.Linear(n_units[i - 1], n_units[i]))
+            layers.append(nn.ELU())
+        layers.append(nn.Linear(n_units[-1], x_dim))
+        self.net = nn.Sequential(*layers)
 
-	def forward(self,z):
-		return self.net(z)
+    def forward(self, z):
+        return self.net(z)
+
 
 class RealNVPLayer(nn.Module):
-	def __init__(self, z_dim):
-		super().__init__()
-		self.z_dim = z_dim
-		self.d1 = np.random.choice(np.arange(1,z_dim))
-		self.d2 = self.z_dim - self.d1
-		self.scale = nn.Sequential(nn.Linear(self.d1,self.d2), nn.ELU(), nn.Linear(self.d2,self.d2))
-		self.shift = nn.Sequential(nn.Linear(self.d1,self.d2), nn.ELU(), nn.Linear(self.d2,self.d2))
+    def __init__(self, z_dim):
+        super().__init__()
+        self.z_dim = z_dim
+        self.d1 = np.random.choice(np.arange(1, z_dim))
+        self.d2 = self.z_dim - self.d1
+        self.scale = nn.Sequential(
+            nn.Linear(self.d1, self.d2), nn.ELU(), nn.Linear(self.d2, self.d2)
+        )
+        self.shift = nn.Sequential(
+            nn.Linear(self.d1, self.d2), nn.ELU(), nn.Linear(self.d2, self.d2)
+        )
 
-	def forward(self,z):
-		# z1 = z.select(dim=-1, np.arange(0,self.d1))
-		# z2 = z.select(dim=-1, np.arange(self.d1,self.z_dim))
-		z1,z2 = z.split([self.d1,self.d2], dim=-1)
-		zout1 = z1
-		zout2 = z2 * self.scale(z1).exp() + self.shift(z1)
-		zout = torch.cat((zout1,zout2), dim=-1)
-		return zout
+    def forward(self, z):
+        # z1 = z.select(dim=-1, np.arange(0,self.d1))
+        # z2 = z.select(dim=-1, np.arange(self.d1,self.z_dim))
+        z1, z2 = z.split([self.d1, self.d2], dim=-1)
+        zout1 = z1
+        zout2 = z2 * self.scale(z1).exp() + self.shift(z1)
+        zout = torch.cat((zout1, zout2), dim=-1)
+        return zout
 
-	def inverse(self,z):
-		z1,z2 = z.split([self.d1,self.d2], dim=-1)
-		zout1 = z1
-		zout2 = (z2 - self.shift(z1))/self.scale(z1).exp()
-		return torch.cat((zout1,zout2), dim=-1)
+    def inverse(self, z):
+        z1, z2 = z.split([self.d1, self.d2], dim=-1)
+        zout1 = z1
+        zout2 = (z2 - self.shift(z1)) / self.scale(z1).exp()
+        return torch.cat((zout1, zout2), dim=-1)
 
-	def jacobian(self,z):
-		z1,_ = z.split([self.d1,self.d2], dim=-1)
-		return self.scale(z1).sum(dim=-1).exp()
+    def jacobian(self, z):
+        z1, _ = z.split([self.d1, self.d2], dim=-1)
+        return self.scale(z1).sum(dim=-1).exp()
+
 
 class PermLayer(nn.Module):
-	def __init__(self, z_dim):
-		super().__init__()
-		self.perm = np.random.permutation(z_dim)
+    def __init__(self, z_dim):
+        super().__init__()
+        self.perm = np.random.permutation(z_dim)
 
-	def forward(self,z):
-		#### Assumes z is (batch, z_dim) !!
-		zout = z[:,self.perm]
-		return zout
+    def forward(self, z):
+        #### Assumes z is (batch, z_dim) !!
+        zout = z[:, self.perm]
+        return zout
 
-	def inverse(self,z):
-		zout = z.new(z.size())
-		zout[:,self_perm] = z
-		return zout
+    def inverse(self, z):
+        zout = z.new(z.size())
+        zout[:, self_perm] = z
+        return zout
 
-	def jacobian(self,z):
-		return 1.0
+    def jacobian(self, z):
+        return 1.0
+
 
 class Flow(nn.Module):
-	def __init__(self, z_dim, n_layers):
-		super().__init__()
-		self.z_dim = z_dim
-		self.n_layers = n_layers
-		self.layers = []
-		for i in range(n_layers):
-			self.layers.append(RealNVPLayer(z_dim))
-			self.layers.append(PermLayer(z_dim))
+    def __init__(self, z_dim, n_layers):
+        super().__init__()
+        self.z_dim = z_dim
+        self.n_layers = n_layers
+        self.layers = []
+        for i in range(n_layers):
+            self.layers.append(RealNVPLayer(z_dim))
+            self.layers.append(PermLayer(z_dim))
 
-	def forward(self,z):
-		for l in self.layers:
-			z = l.forward(z)
-		return z
+    def forward(self, z):
+        for l in self.layers:
+            z = l.forward(z)
+        return z
 
-	def inverse(self,z):
-		for l in self.layers.reverse():
-			z = l.inverse(z)
-		return z
+    def inverse(self, z):
+        for l in self.layers.reverse():
+            z = l.inverse(z)
+        return z
 
-	def jacobian(self,z):
-		J = 1.0
-		for l in self.layers:
-			J *= l.jacobian(z)
-		return J
+    def jacobian(self, z):
+        J = 1.0
+        for l in self.layers:
+            J *= l.jacobian(z)
+        return J
 
 
 class Discriminator(nn.Module):
-	def __init__(self, z_dim, n_units):
-		super().__init__()
-		self.z_dim = z_dim
-		self.n_units = n_units
-		layers = [nn.Linear(z_dim, n_units[0]), nn.ELU()]
-		for i in range(1,len(n_units)):
-			layers.append(nn.Linear(n_units[i-1],n_units[i]))
-			layers.append(nn.ELU())
-		layers.append(nn.Linear(n_units[-1],1))
-		self.net = nn.Sequential(*layers)
+    def __init__(self, z_dim, n_units):
+        super().__init__()
+        self.z_dim = z_dim
+        self.n_units = n_units
+        layers = [nn.Linear(z_dim, n_units[0]), nn.ELU()]
+        for i in range(1, len(n_units)):
+            layers.append(nn.Linear(n_units[i - 1], n_units[i]))
+            layers.append(nn.ELU())
+        layers.append(nn.Linear(n_units[-1], 1))
+        self.net = nn.Sequential(*layers)
 
-	def forward(self,z):
-		return self.net(z)
+    def forward(self, z):
+        return self.net(z)
+
--- train_mnist.py
+++ train_mnist.py
@@ -7,34 +7,40 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import models
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device ='cpu'
+
 
 def get_mnist_data(device):
     preprocess = transforms.ToTensor()
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('mnist_data', train=True, download=True, transform=preprocess),
+        datasets.MNIST("mnist_data", train=True, download=True, transform=preprocess),
         batch_size=100,
-        shuffle=True)
+        shuffle=True,
+    )
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('mnist_data', train=False, download=True, transform=preprocess),
+        datasets.MNIST("mnist_data", train=False, download=True, transform=preprocess),
         batch_size=100,
-        shuffle=True)
+        shuffle=True,
+    )
 
     return train_loader, test_loader
+
 
 train_loader, test_loader = get_mnist_data(device)
 num_epochs = 10
 x_dim = 784
 z_dim = 20
 
-encoder = models.Encoder(x_dim, z_dim, [300,300])
-decoder = models.Decoder(z_dim, x_dim, [300,300])
+encoder = models.Encoder(x_dim, z_dim, [300, 300])
+decoder = models.Decoder(z_dim, x_dim, [300, 300])
 flow = models.Flow(z_dim, 3)
-discriminator = models.Discriminator(z_dim, [10,10])
+discriminator = models.Discriminator(z_dim, [10, 10])
 g_optimizer = optim.Adam(flow.parameters(), lr=1e-3)
 d_optimizer = optim.Adam(discriminator.parameters(), lr=1e-3)
-ed_optimizer = optim.Adam(list(encoder.parameters())+list(decoder.parameters()), lr=1e-3)
+ed_optimizer = optim.Adam(
+    list(encoder.parameters()) + list(decoder.parameters()), lr=1e-3
+)
 
 
 # pbar = tqdm(range(num_epochs))
@@ -42,25 +48,31 @@ for e in range(num_epochs):
     for batch in train_loader:
         x_batch, y_batch = batch
         batch_size = x_batch.size()[0]
-        x_batch = x_batch.to(device).reshape(-1,784).float() / 255
+        x_batch = x_batch.to(device).reshape(-1, 784).float() / 255
         labels = y_batch.new(np.eye(10)[y_batch.cpu()]).to(device).float()
-        
+
         ## Train discriminator on real z's and fake z's
         ## min -log(G(real)) - log(1-G(fake))
         d_optimizer.zero_grad()
         real_z = encoder(x_batch).detach()
-        real_loss = torch.nn.BCEWithLogits()(input=discriminator(real_z), target=torch.ones(batch_size,1).to(device)).sum()
+        real_loss = torch.nn.BCEWithLogits()(
+            input=discriminator(real_z), target=torch.ones(batch_size, 1).to(device)
+        ).sum()
         real_loss.backward()
-        fake_z = flow(torch.rand(batch_size,z_dim)).detach()
-        fake_loss = torch.nn.BCEWithLogits()(input=discriminator(fake_z), target=torch.zeros(batch_size,1).to(device)).sum()
+        fake_z = flow(torch.rand(batch_size, z_dim)).detach()
+        fake_loss = torch.nn.BCEWithLogits()(
+            input=discriminator(fake_z), target=torch.zeros(batch_size, 1).to(device)
+        ).sum()
         fake_loss.backward()
         d_optimizer.step()
 
         ## Train generator
         ## min -log(G(fake))
         g_optimizer.zero_grad()
-        fake_z = flow(torch.rand(batch_size,z_dim))
-        gen_loss = torch.nn.BCEWithLogits()(input=discriminator(real_z), target=torch.ones(batch_size,1).to(device)).sum()
+        fake_z = flow(torch.rand(batch_size, z_dim))
+        gen_loss = torch.nn.BCEWithLogits()(
+            input=discriminator(real_z), target=torch.ones(batch_size, 1).to(device)
+        ).sum()
         gen_loss.backward()
         g_optimizer.step()
 
@@ -73,4 +85,9 @@ for e in range(num_epochs):
         ed_optimizer.step()
 
         # pbar.set_postfix(loss='{:.2e}'.format(loss))
-    print("Epoch {} : Disc loss = {:.2e} Gen loss = {:.2e} E-D loss = {:.2e}".format(e,real_loss+fake_loss,gen_loss,ed_loss))    
+    print(
+        "Epoch {} : Disc loss = {:.2e} Gen loss = {:.2e} E-D loss = {:.2e}".format(
+            e, real_loss + fake_loss, gen_loss, ed_loss
+        )
+    )
+
